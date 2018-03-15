e1e9dec20bdc68e3b5550dcba69f4fa2f1d5b3a4
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-15 11:21:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Write your solution in this file!
 var customerName = 'bob'
+let bestCustomer = 'denis'
 
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  
 }
